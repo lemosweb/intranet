1b236e73b47c77eb68cbd4977b5af492adbb8998
Doniex Lemavorum <daniel.o.lemos@hotmail.com>
2016-05-21 19:03:48 -0300

    msg

--- database/migrations/2016_05_20_144903_create_contatos_table.php
+++ database/migrations/2016_05_20_144903_create_contatos_table.php
@@ -12,7 +12,7 @@ class CreateContatosTable extends Migration
      */
     public function up()
     {
-        /*DB::statement('set foreign_key_checks = 0;');*/
+        DB::statement('set foreign_key_checks = 0;');
         Schema::create('contatos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nome',255);
@@ -26,7 +26,7 @@ class CreateContatosTable extends Migration
 
             $table->timestamps();
         });
-        /*DB::statement('set foreign_key_checks = 1;');*/
+        DB::statement('set foreign_key_checks = 1;');
     }
 
     /**
